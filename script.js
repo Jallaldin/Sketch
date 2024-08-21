9f3e6105ab45134319b978778deb335186710ce2
Jallaldin Hamdard <jalladinhamdard@gmail.com>
2024-08-21 13:43:04 +0100

    buttons added + change colour when hovered

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
+
 const gridContainer = document.querySelector('.gridContainer');
 
 
-function Squares() {
-    const squareGrid = 16;
-    for (let i = 0; i < squareGrid; i++) {
+
+function squareGrid(num) {
+    const gridOfSquares = num * num;
+    gridContainer.style.gridTemplateColumns = `repeat(${num}, 1fr)`;
+    gridContainer.style.gridTemplateRows = `repeat(${num}, 1fr)`;
+    for (let i = 0; i < gridOfSquares; i++) {
         const square = document.createElement('div');
         square.classList.add('square');
         gridContainer.appendChild(square);
     }
+    const grid = document.querySelector('.grid');
+    grid.textContent = `${num} x ${num}`;
 
+}
+
+function setColour() {
+    const squares = document.querySelectorAll('.square');
+    squares.forEach(square => {
+        square.addEventListener('mouseover', (event) => {
+            square.style.backgroundColor = 'black';
+        });
+    });
 }
 
 Squares();
